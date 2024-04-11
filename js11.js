b770417d7635a9d0783825a34c5715bcadb77250
Yiyu Pan <164454201+ElodyPan@users.noreply.github.com>
2024-04-11 17:48:48 -0400

    Add files via upload

---
+++ js11.js
@@ -0,0 +1,97 @@
+
+mapboxgl.accessToken = 'pk.eyJ1IjoiZWxvZHlwIiwiYSI6ImNsdWx2dWQzaDBuaGkybG13cmJhbHBia2MifQ.6QH1q00S5v6MYczAiRLy0g';
+
+var map = new mapboxgl.Map({
+    style: 'mapbox://styles/mapbox/dark-v11',
+    center: [-74.00050, 40.73001],
+    zoom: 11,
+    pitch: 45,
+    bearing: -17.6,
+    container: 'map-container',
+    antialias: true
+});
+
+map.on('style.load', () => {
+    // Insert the layer beneath any symbol layer.
+    const layers = map.getStyle().layers;
+    const labelLayerId = layers.find(
+        (layer) => layer.type === 'symbol' && layer.layout['text-field']
+    ).id;
+
+    // The 'building' layer in the Mapbox Streets
+    // vector tileset contains building height data
+    // from OpenStreetMap.
+    map.addLayer(
+        {
+            'id': 'add-3d-buildings',
+            'source': 'composite',
+            'source-layer': 'building',
+            'filter': ['==', 'extrude', 'true'],
+            'type': 'fill-extrusion',
+            'minzoom': 15,
+            'paint': {
+                'fill-extrusion-color': '#aaa',
+
+                // Use an 'interpolate' expression to
+                // add a smooth transition effect to
+                // the buildings as the user zooms in.
+                'fill-extrusion-height': [
+                    'interpolate',
+                    ['linear'],
+                    ['zoom'],
+                    15,
+                    0,
+                    15.05,
+                    ['get', 'height']
+                ],
+                'fill-extrusion-base': [
+                    'interpolate',
+                    ['linear'],
+                    ['zoom'],
+                    15,
+                    0,
+                    15.05,
+                    ['get', 'min_height']
+                ],
+                'fill-extrusion-opacity': 0.6
+            }
+        },
+        labelLayerId
+    );
+});
+
+map.on('load', function () {
+    ratingData.forEach(function (building) {
+        var el = document.createElement('div');
+        el.className = 'marker';
+        el.style.backgroundColor = getGradeColor(building["ENERGY EFFICIENCY GRADE"]);
+
+        var popupContent = `
+            <h3>${building["STREET NAME"]}</h3>
+            <p><strong>DOF Gross Square Footage:</strong> ${building["DOF GROSS SQUARE FOOTAGE"].toLocaleString()} sq ft</p>
+            <p><strong>Energy Star Score:</strong> ${building["ENERGY STAR 1-100 SCORE"]}</p>
+            <p><strong>Energy Efficiency Grade:</strong> ${building["ENERGY EFFICIENCY GRADE"]}</p>
+        `;
+
+        // Create a popup
+        var popup = new mapboxgl.Popup({ offset: 25 }).setHTML(popupContent);
+
+        // Add the marker to the map with the popup
+        new mapboxgl.Marker(el)
+            .setLngLat([building.longitude, building.latitude])
+            .setPopup(popup)
+            .addTo(map);
+    });
+});
+
+function getGradeColor(grade) {
+    switch (grade) {
+        case 'A': return '#08922d'; // dark green
+        case 'B': return '#6fd884'; // light green
+        case 'C': return '#e6f4ea'; // pale green
+        case 'D': return '#c3ab91'; // beige
+        case 'F': return '#e1861d'; // orange
+        case 'N': return '#5b5957'; // dark grey
+        default:  return '#ffffff'; // white for undefined grades
+    }
+}
---
+++ rating-data.js
@@ -0,0 +1,262 @@
+var ratingData = [
+    {
+      "10-Digit BBL": 1000020002,
+      "STREET NO.": 10,
+      "STREET NAME": "SOUTH STREET",
+      "DOF GROSS SQUARE FOOTAGE": 158197,
+      "ENERGY STAR 1-100 SCORE": "Required benchmarking information missing",
+      "ENERGY EFFICIENCY GRADE": "F",
+      "latitude": 40.70921,
+      "longitude": -73.99481
+    },
+    {
+      "10-Digit BBL": 1000047501,
+      "STREET NO.": 1,
+      "STREET NAME": "WATER STREET",
+      "DOF GROSS SQUARE FOOTAGE": 2542563,
+      "ENERGY STAR 1-100 SCORE": 63,
+      "ENERGY EFFICIENCY GRADE": "C",
+      "latitude": 40.70272,
+      "longitude": -73.985191
+    },
+    {
+      "10-Digit BBL": 1000050010,
+      "STREET NO.": 115,
+      "STREET NAME": "BROAD STREET",
+      "DOF GROSS SQUARE FOOTAGE": 1016406,
+      "ENERGY STAR 1-100 SCORE": 91,
+      "ENERGY EFFICIENCY GRADE": "A",
+      "latitude": 40.702091,
+      "longitude": -74.012016
+    },
+    {
+      "10-Digit BBL": 1000057501,
+      "STREET NO.": 125,
+      "STREET NAME": "BROAD STREET",
+      "DOF GROSS SQUARE FOOTAGE": 1354691,
+      "ENERGY STAR 1-100 SCORE": 85,
+      "ENERGY EFFICIENCY GRADE": "A",
+      "latitude": 40.702091,
+      "longitude": -74.012016
+    },
+    {
+      "10-Digit BBL": 1000080039,
+      "STREET NO.": 32,
+      "STREET NAME": "PEARL STREET",
+      "DOF GROSS SQUARE FOOTAGE": 46724,
+      "ENERGY STAR 1-100 SCORE": "Required benchmarking information missing",
+      "ENERGY EFFICIENCY GRADE": "F",
+      "latitude": 40.702697,
+      "longitude": -73.988049
+    },
+    {
+      "10-Digit BBL": 1000080051,
+      "STREET NO.": 6,
+      "STREET NAME": "WATER STREET",
+      "DOF GROSS SQUARE FOOTAGE": 102407,
+      "ENERGY STAR 1-100 SCORE": 26,
+      "ENERGY EFFICIENCY GRADE": "D",
+      "latitude": 40.70272,
+      "longitude": -73.985191
+    },
+    {
+      "10-Digit BBL": 1000087501,
+      "STREET NO.": 39,
+      "STREET NAME": "WHITEHALL STREET",
+      "DOF GROSS SQUARE FOOTAGE": 169061,
+      "ENERGY STAR 1-100 SCORE": 32,
+      "ENERGY EFFICIENCY GRADE": "D",
+      "latitude": 40.702091,
+      "longitude": -74.012016
+    },
+    {
+      "10-Digit BBL": 1000090001,
+      "STREET NO.": 34,
+      "STREET NAME": "WHITEHALL STREET",
+      "DOF GROSS SQUARE FOOTAGE": 692431,
+      "ENERGY STAR 1-100 SCORE": 83,
+      "ENERGY EFFICIENCY GRADE": "B",
+      "latitude": 40.702091,
+      "longitude": -74.012016
+    },
+    {
+      "10-Digit BBL": 1000090014,
+      "STREET NO.": 17,
+      "STREET NAME": "STATE STREET",
+      "DOF GROSS SQUARE FOOTAGE": 544015,
+      "ENERGY STAR 1-100 SCORE": 75,
+      "ENERGY EFFICIENCY GRADE": "B",
+      "latitude": 40.70261,
+      "longitude": -74.01368
+    },
+    {
+      "10-Digit BBL": 1000090029,
+      "STREET NO.": 24,
+      "STREET NAME": "STATE STREET",
+      "DOF GROSS SQUARE FOOTAGE": 896956,
+      "ENERGY STAR 1-100 SCORE": 56,
+      "ENERGY EFFICIENCY GRADE": "C",
+      "latitude": 40.70261,
+      "longitude": -74.01368
+    },
+    {
+      "10-Digit BBL": 1000100014,
+      "STREET NO.": 33,
+      "STREET NAME": "WHITEHALL STREET",
+      "DOF GROSS SQUARE FOOTAGE": 365792,
+      "ENERGY STAR 1-100 SCORE": 82,
+      "ENERGY EFFICIENCY GRADE": "B",
+      "latitude": 40.702091,
+      "longitude": -74.012016
+    },
+    {
+      "10-Digit BBL": 1000100016,
+      "STREET NO.": 90,
+      "STREET NAME": "BROAD STREET",
+      "DOF GROSS SQUARE FOOTAGE": 336025,
+      "ENERGY STAR 1-100 SCORE": 79,
+      "ENERGY EFFICIENCY GRADE": "B",
+      "latitude": 40.702091,
+      "longitude": -74.012016
+    },
+    {
+      "10-Digit BBL": 1000100023,
+      "STREET NO.": 1,
+      "STREET NAME": "WHITEHALL STREET",
+      "DOF GROSS SQUARE FOOTAGE": 321994,
+      "ENERGY STAR 1-100 SCORE": 87,
+      "ENERGY EFFICIENCY GRADE": "A",
+      "latitude": 40.702091,
+      "longitude": -74.012016
+    },
+    {
+      "10-Digit BBL": 1000110021,
+      "STREET NO.": 80,
+      "STREET NAME": "BROAD STREET",
+      "DOF GROSS SQUARE FOOTAGE": 361710,
+      "ENERGY STAR 1-100 SCORE": 80,
+      "ENERGY EFFICIENCY GRADE": "B",
+      "latitude": 40.702091,
+      "longitude": -74.012016
+    },
+    {
+      "10-Digit BBL": 1000130001,
+      "STREET NO.": 1,
+      "STREET NAME": "BROADWAY",
+      "DOF GROSS SQUARE FOOTAGE": 180646,
+      "ENERGY STAR 1-100 SCORE": 87,
+      "ENERGY EFFICIENCY GRADE": "A",
+      "latitude": 40.695467,
+      "longitude": -73.932078
+    },
+    {
+      "10-Digit BBL": 1000130005,
+      "STREET NO.": 11,
+      "STREET NAME": "BROADWAY",
+      "DOF GROSS SQUARE FOOTAGE": 434220,
+      "ENERGY STAR 1-100 SCORE": 92,
+      "ENERGY EFFICIENCY GRADE": "A",
+      "latitude": 40.695467,
+      "longitude": -73.932078
+    },
+    {
+      "10-Digit BBL": 1000130027,
+      "STREET NO.": 25,
+      "STREET NAME": "BROADWAY",
+      "DOF GROSS SQUARE FOOTAGE": 809401,
+      "ENERGY STAR 1-100 SCORE": 65,
+      "ENERGY EFFICIENCY GRADE": "C",
+      "latitude": 40.695467,
+      "longitude": -73.932078
+    },
+    {
+      "10-Digit BBL": 1000150022,
+      "STREET NO.": 21,
+      "STREET NAME": "WEST STREET",
+      "DOF GROSS SQUARE FOOTAGE": 335746,
+      "ENERGY STAR 1-100 SCORE": 29,
+      "ENERGY EFFICIENCY GRADE": "D",
+      "latitude": 40.68253,
+      "longitude": -73.909294
+    },
+    {
+      "10-Digit BBL": 1000157501,
+      "STREET NO.": 17,
+      "STREET NAME": "BATTERY PLACE",
+      "DOF GROSS SQUARE FOOTAGE": 1402831,
+      "ENERGY STAR 1-100 SCORE": 81,
+      "ENERGY EFFICIENCY GRADE": "B",
+      "latitude": 40.706009,
+      "longitude": -74.018602
+    },
+    {
+      "10-Digit BBL": 1000157502,
+      "STREET NO.": 20,
+      "STREET NAME": "WEST STREET",
+      "DOF GROSS SQUARE FOOTAGE": 259076,
+      "ENERGY STAR 1-100 SCORE": 78,
+      "ENERGY EFFICIENCY GRADE": "B",
+      "latitude": 40.68253,
+      "longitude": -73.909294
+    },
+    {
+      "10-Digit BBL": 1000160015,
+      "STREET NO.": 50,
+      "STREET NAME": "BATTERY PLACE",
+      "DOF GROSS SQUARE FOOTAGE": 238165,
+      "ENERGY STAR 1-100 SCORE": 80,
+      "ENERGY EFFICIENCY GRADE": "B",
+      "latitude": 40.706009,
+      "longitude": -74.018602
+    },
+    {
+      "10-Digit BBL": 1000160020,
+      "STREET NO.": 70,
+      "STREET NAME": "BATTERY PLACE",
+      "DOF GROSS SQUARE FOOTAGE": 230765,
+      "ENERGY STAR 1-100 SCORE": 82,
+      "ENERGY EFFICIENCY GRADE": "B",
+      "latitude": 40.706009,
+      "longitude": -74.018602
+    },
+    {
+      "10-Digit BBL": 1000160100,
+      "STREET NO.": 345,
+      "STREET NAME": "SOUTH END AVENUE",
+      "DOF GROSS SQUARE FOOTAGE": 1881621,
+      "ENERGY STAR 1-100 SCORE": 72,
+      "ENERGY EFFICIENCY GRADE": "B",
+      "latitude": 40.710949,
+      "longitude": -74.016495
+    },
+    {
+      "10-Digit BBL": 1000160120,
+      "STREET NO.": 200,
+      "STREET NAME": "LIBERTY STREET",
+      "DOF GROSS SQUARE FOOTAGE": 1501878,
+      "ENERGY STAR 1-100 SCORE": 86,
+      "ENERGY EFFICIENCY GRADE": "A",
+      "latitude": 40.707406,
+      "longitude": -74.008185
+    },
+    {
+      "10-Digit BBL": 1000160125,
+      "STREET NO.": 225,
+      "STREET NAME": "LIBERTY STREET",
+      "DOF GROSS SQUARE FOOTAGE": 2267925,
+      "ENERGY STAR 1-100 SCORE": 77,
+      "ENERGY EFFICIENCY GRADE": "B",
+      "latitude": 40.707406,
+      "longitude": -74.008185
+    },
+    {
+      "10-Digit BBL": 1000160140,
+      "STREET NO.": 200,
+      "STREET NAME": "VESEY STREET",
+      "DOF GROSS SQUARE FOOTAGE": 2233692,
+      "ENERGY STAR 1-100 SCORE": "Required benchmarking information missing",
+      "ENERGY EFFICIENCY GRADE": "F",
+      "latitude": 40.713573,
+      "longitude": -74.014883
+    }
+  ]
